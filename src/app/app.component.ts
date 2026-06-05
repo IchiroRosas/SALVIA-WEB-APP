e1052a69f1f5a40f8a21c25562f965ddd8eea84e
IchiroRosas <rosaschinenraulichiro@gmail.com>
2026-06-04 23:33:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Salvia-Web';
+  firestore = inject(Firestore);
+
+  ngOnInit(): void {
+    const categoriasCollection = collection(this.firestore, 'categorias');
+    console.log('¡Firestore está conectado correctamente!', categoriasCollection);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBR0P9NkxlFwJ9z6vAqHsuOg5fhVRJj6gA",
+        authDomain: "salvia-app-865f5.firebaseapp.com",
+        projectId: "salvia-app-865f5",
+        storageBucket: "salvia-app-865f5.firebasestorage.app",
+        messagingSenderId: "572457901891",
+        appId: "1:572457901891:web:546fa1e4ec7168bae819f6",
+        measurementId: "G-T1D4PMLBZS"
+    },
+    geminiApiKey: "TU_API_KEY_DE_GEMINI_AQUI"
+};
