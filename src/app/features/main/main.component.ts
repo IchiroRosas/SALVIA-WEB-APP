316156b01173a926888bc8e04ef75d10f44018bb
Nicole Aguilar <nivvic.3006@gmail.com>
2026-06-15 23:30:21 -0500

    main menu front finalizado y componentes creados

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // 1. Importa FormsModule para usar [(ngModel)]
+import { CurrencyPipe } from '@angular/common'; // Opcional: para darle formato de dinero automáticamente
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule,   // 2. Agrégalo aquí
+    CurrencyPipe   // 3. Agrégalo aquí si quieres formato automático
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrls: ['./main.component.css']
 })
 export class MainComponent {
-
+  // Estado para controlar si se está editando o no
+  isEditingStartBalance = false;
+  
+  // Valor inicial del balance (guárdalo como número)
+  startBalance: number = 45000; 
 }
---
+++ src/app/features/reportes-historial/reportes-historial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reportes-historial',
+  standalone: true,
+  imports: [],
+  templateUrl: './reportes-historial.component.html',
+  styleUrl: './reportes-historial.component.css'
+})
+export class ReportesHistorialComponent {
+
+}
