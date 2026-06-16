b76ee73cca5c005debe85a5f95ab31808f68e08f
IchiroRosas <rosaschinenraulichiro@gmail.com>
2026-06-15 20:14:53 -0500

    nuevo flujo login componentes creados

---
+++ src/app/core/activate-user/activate-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activate-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './activate-user.component.html',
+  styleUrl: './activate-user.component.css'
+})
+export class ActivateUserComponent {
+
+}
---
+++ src/app/core/register-company-first-admin-user/register-company-first-admin-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-company-first-admin-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-company-first-admin-user.component.html',
+  styleUrl: './register-company-first-admin-user.component.css'
+})
+export class RegisterCompanyFirstAdminUserComponent {
+
+}
